6ba247a890a9f684fe7bc8593a0a255c71f6370b
lasha <lashacst@gmail.com>
2016-03-22 13:11:47 +0400

    html5 module update - using ES6 features

--- modules/html5.js
+++ modules/html5.js
@@ -2,7 +2,7 @@
  * @file html5-audit.js
  * @author Lasha Badashvili (lashab@picktek.com)
  *
- * Scan HTML files (using html5-lint module).
+ * HTML5 validator (using html5-lint module).
  */
 
 'use strict';
@@ -10,73 +10,69 @@
 /**
  * Module dependencies.
  */
-var fs = require('fs');
-var html5Lint = require('html5-lint');
-var colors = require('colors');
-var _ = require('underscore');
-var report = require('../helpers/report');
-var files = require('../helpers/files');
+const fs = require('fs');
+const html5Lint = require('html5-lint');
+const colors = require('colors');
+const _ = require('underscore');
+const report = require('../helpers/report');
+const files = require('../helpers/files');
 
-module.exports = function(argv) {
-  // Help text.
-  var help = 'html-audit html5 usage:\n' +
-    '\thtml-audit html5 [options]\n' +
-    'Options\n' +
-    '\t--help                                                               ' +
-    'Display this error message\n' +
-    '\t--path           [path / file] (required)                            ' +
-    'Path to HTML files or an HTML file to audit\n' +
-    '\t--base-uri       [URI]         (required)                            ' +
-    'The base URL of the site being audited\n' +
-    '\t--report         [path]                                              ' +
-    'Path to output JSON audit report\n' +
-    '\t--report-verbose                                                     ' +
-    'Verbose report\n' +
-    '\t--map            [file]        (required when --lastmod is provided) ' +
-    'File containing filename:url object\n' +
-    '\t--lastmod                                                            ' +
-    'Scan last modified files';
+module.exports = (argv) => {
+  /*eslint-disable max-len*/
+  // Prepare help text.
+  const help = `html-audit html5 usage:
+        html-audit html5 [options]
+Options
+        --help                                                                Display help text
+        --path           [path / file] (required)                             Path to HTML files or an HTML file to audit
+        --report         [path]                                               Path to output JSON audit report
+        --errors-only                                                         Only report errors (no notices or warnings)
+        --map            [file]        (required when --lastmod is provided)  JSON map file which holds modified files data
+        --lastmod                                                             Scan last modified files
+`;
+  /*eslint-enable max-len*/
 
   if (argv.help) {
-    process.stdout.write(help.yellow + '\n');
+    process.stdout.write(help.yellow);
     process.exit(0);
   }
 
-  var _data = [];
-  // Get path to file.
-  var path = argv.path;
-  // Get report directory.
-  var _report = argv.report || '';
-  // Get errors only.
-  var errorsOnly = argv['errors-only'] || false;
-  // Get JSON map path.
-  var map = argv.map || '';
-  // Get modified boolean.
-  var modified = argv.lastmod || false;
+  const _data = [];
+  // Get arg - path to file.
+  const path = argv.path;
+  // Get arg - report directory.
+  const _report = argv.report || '';
+  // Get arg - errors only.
+  const errorsOnly = argv['errors-only'] || false;
+  // Get arg -JSON map file.
+  const map = argv.map || '';
+  // Get arg - modified boolean.
+  const modified = argv.lastmod || false;
+
   if (!path || (modified && !map)) {
-    process.stdout.write(help.yellow + '\n');
+    process.stdout.write(help.yellow);
     process.exit(0);
   }
 
-  var i = 1;
+  let i = 1;
   // Get file(s).
-  files(path, argv._, map, modified, function(file, length) {
-    // Get content.
-    fs.readFile(file, 'utf-8', function(error, content) {
+  files(path, argv._, map, modified, (file, length) => {
+    // Get file content.
+    fs.readFile(file, 'utf-8', (error, content) => {
       if (error) {
         throw new Error(error);
       }
+
       // Test file.
-      html5Lint(content, {
-        errorsOnly: errorsOnly
-      }, function(error, data) {
+      html5Lint(content, { errorsOnly }, (error, data) => {
         if (error) {
           throw new Error(error);
         }
+
         if (data.messages.length) {
           // Log file scanning.
           console.log('Scanning %s'.green, file);
-          data.messages.forEach(function(object) {
+          data.messages.forEach((object) => {
             // Add filename.
             object.filename = file;
             // Store result in _data variable.
@@ -94,3 +90,4 @@ module.exports = function(argv) {
     });
   });
 };
+
