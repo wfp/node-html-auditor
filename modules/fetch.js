56d0447b01ab12158df98dcbb50a4c7d3cf49680
lasha <lashacst@gmail.com>
2016-03-24 15:59:35 +0400

    Make fetch module testable v1

--- modules/fetch.js
+++ modules/fetch.js
@@ -18,72 +18,143 @@ const request = require('request');
 const colors = require('colors');
 const mkdirp = require('mkdirp');
 
-module.exports = (argv) => {
-  /*eslint-disable max-len*/
-  // Prepare help text.
-  const help = `html-audit fetch usage:
-        html-audit fetch [options]
-Options
-        --help                                                    Display help text
-        --uri      [URI]  (required)                              Path or URL to XML Sitemap file
-        --dir      [path] (required)                              Directory to output HTML files
-        --map      [file] (required  when --lastmod is provided)  File to output JSON that maps file names to URLs and modified files. If not set, sends to stdout
-        --lastmod  [date]                                         Date for downloading last modified content
-`;
-  /*eslint-enable max-len*/
+module.exports = {
+  /**
+   * Execute fetch.
+   */
+  execute: function(argv) {
+    // Get arg - sitemap uri.
+    const uri = argv.uri;
+    // Get arg - sitemap htmls directory.
+    const dir = argv.dir;
+    // Get arg - JSON map file.
+    const map = argv.map;
+    // Get arg - last modified date.
+    const modified = argv.lastmod || '';
 
-  // Get arg - sitemap uri.
-  const uri = argv.uri;
-  // Get arg - sitemap htmls directory.
-  let dir = argv.dir;
-  // Get arg - JSON map file.
-  const map = argv.map;
-  // Get arg - last modified date.
-  const modified = argv.lastmod || '';
+    if (argv.help || (!uri || !dir) || (modified && !map)) {
+      process.stdout.write(this.help());
+      process.exit(0);
+    }
 
-  if (argv.help || (!uri || !dir) || (modified && !map)) {
-    process.stdout.write(help.yellow);
-    process.exit(0);
-  }
+    // Make http request for sitemap uri.
+    this.request(uri, (error, status, XML) => {
+      status = response.statusCode || false;
+      if (error) {
+        throw new Error(error);
+      }
+      else if (status && status !== 200) {
+        throw new Error(`${uri} not found - status code: ${status}`);
+      }
 
-  dir = path.resolve(dir);
+      let i = 0;
+      const _map = {
+        uris: {},
+        modified: []
+      };
+
+      // Get uris [<loc>] from XML. 
+      this.getUrisFromXML(XML, modified, (error, uri, modified) => {
+        if (error) {
+          throw new Error(error);
+        }
+
+        // Make http request for [<loc>] uri.
+        this.request(uri, (error, status, HTML) => {
+          if (error) {
+            // Log & skip.
+            console.dir(error);
+          }
+
+          i++;
+          // Add sitemap-[ID].html.
+          this.addSitemap(HTML, `sitemap-${i}.html`, dir, (error, basename, file) => {
+            if (error) {
+              throw new Error(error);
+            }
+
+            _map.uris[basename] = uri;
+
+            if (modified) {
+              _map.modified.push(file);
+            }
+
+            console.log(`${file}.green has been added`);
+
+            if (Object.keys(_map.uris).length === i) {
+              // Add sitemap map | Log.
+              this.addSitemapMap(_map, map, (error, mapJSON) => {
+                if (error) {
+                  throw new Error(error);
+                }
+
+                // Log.
+                console.log(`${mapJSON}.green has been added
+                `);
+
+                // Log.
+                console.log('Fetch has completed'.green);
+              });
+            }
+          });
+        });
+      });
+    });
+  },
 
   /**
-   * Make http request for sitemap uri and get XML.
+   * Get help text.
+   */
+  help: () => {
+    /*eslint-disable max-len*/
+    const help = `html-audit fetch usage:
+          html-audit fetch [options]
+  Options
+          --help                                                    Display help text
+          --uri      [URI]  (required)                              Path or URL to XML Sitemap file
+          --dir      [path] (required)                              Directory to output HTML files
+          --map      [file] (required  when --lastmod is provided)  File to output JSON that maps file names to URLs and modified files. If not set, sends to stdout
+          --lastmod  [date]                                         Date for downloading last modified content
+  `;
+    /*eslint-enable max-len*/
+
+    return help.yellow;
+  },
+
+  /**
+   * Make http request.
    *
+   * @param {String} uri 
    * @param {Function} callback
    */
-  const getSitemapXML = (callback) => {
-    let XML = '';
+  request: (uri, callback) => {
+    let content = '';
     request(uri, (error, response, body) => {
-      if (error) {
-        callback(error);
-      }
-
-      if (response.statusCode !== 200) {
-        callback(`${uri} not found - status code: ${response.statusCode}`);
-      }
+      callback(error, response.statusCode);
     }).on('data', (data) => {
-      XML += data;
+      XML = data;
     }).on('end', () => {
-      callback(null, XML);
+      if (content) {
+        callback(null, null, content);
+      }
     }).on('error', (error) => {
       callback(error);
     }).end();
-  };
+  },
 
   /**
-   * Get uris (<loc>) from XML.
+   * Get uris [<loc>] from XML.
    *
    * @param {String} XML
+   * @param {Boolean} modified
    * @param {Function} callback
    */
-  const getUrisFromXML = (XML, callback) => {
+  getUrisFromXML: (XML, modified, callback) => {
     const _stream = new stream.PassThrough();
     _stream.end(XML);
     const _XML =  new XMLStream(_stream);
-    let _modified = false;
     _XML.on('endElement: url', (item) => {
+      let _modified = false;
       let _uri = '';
       if ('loc' in item) {
         // Get location (uri).
@@ -96,16 +167,15 @@ Options
           const condition = __modified === ___modified ||
             __modified > ___modified;
           if (condition) {
-            _modified = true;
             _uri = location;
+            _modified = true;
           }
         }
         else if (modified && !('lastmod' in item)) {
-          _modified = true;
           _uri = location;
+          _modified = true;
         }
         else {
-          _modified = false;
           _uri = location;
         }
       }
@@ -118,98 +188,77 @@ Options
         callback(null, _uri, _modified);
       }
     });
-  };
+  },
 
   /**
-   * Get sitemap HTML content from uri & store in file.
+   * Add sitemap file.
    *
-   * @param {String} uri
-   * @param {Number} key
+   * @param {Object} data
+   * @param {String} basename
+   * @param {String} directory
    * @param {Function} callback
    */
-  const getSitemapContent = (uri, key, callback) => {
+  addSitemap: (data, basename, directory, callback) => {
+    directory = path.resolve(directory);
     // Create sitemap htmls directory.
-    mkdirp(dir, '0777', (error) => {
+    mkdirp(directory, '0777', (error) => {
       if (error) {
         callback(error);
       }
 
-      // Prepare unique filename.
-      const filename = `sitemap-${key}.html`;
-      // Create file.
-      const stream = fs.createWriteStream(path.join(dir, filename));
-      // Make HTTP request for each of the uri & get HTML content.
-      request(uri).on('data', (data) => {
-        // Write HTML content in file.
-        stream.write(data);
-      }).on('end', () => {
-        // Log.
-        console.log(`${filename.green} has been added`);
-        callback(null, filename);
-      }).on('error', (error) => {
+      const file = path.join(directory, basename);
+      // Stream - create sitemap file.
+      const stream = fs.createWriteStream(file);
+      // Stream - error event.
+      stream.on('error', (error) => {
         callback(error);
-      }).end();
-    });
-  };
-
-  getSitemapXML((error, XML) => {
-    if (error) {
-      throw new Error(error);
-    }
-
-    let i = 0;
-    const _map = {
-      uris: {},
-      modified: []
-    };
-    getUrisFromXML(XML, (error, uri, modified) => {
-      if (error) {
-        throw new Error(error);
-      }
-
-      getSitemapContent(uri, i, (error, filename) => {
-        if (error) {
-          throw new Error(error);
-        }
-
-        _map.uris[filename] = uri;
-
-        if (modified) {
-          _map.modified.push(path.join(dir, filename));
-        }
-
-        if (Object.keys(_map.uris).length === i) {
-          if (map && typeof map === 'string') {
-            // Get map directory.
-            const dirname = path.dirname(path.normalize(map));
-            // Create map directory.
-            mkdirp(dirname, '0777', (error) => {
-              if (error) {
-                throw new Error(error);
-              }
-
-              // Get map basename.
-              const basename = path.basename(map).split('.');
-              // Prepare [MAP].json file.
-              const mapJSON = path.join(dirname, `${basename[0]}.json`);
-              // Create map file.
-              // - filename:url object.
-              // - last modified file paths object.
-              const stream = fs.createWriteStream(mapJSON);
-              stream.write(JSON.stringify(_map));
-            });
-          }
-          else {
-            // Log.
-            console.dir(_map);
-          }
-
-          // Log.
-          console.log('Download has completed'.green);
-        }
       });
-      i++;
-    });
-  });
-};
 
+      // Stream - write in file.
+      stream.write(data);
+
+      callback(null, basename, file);
+    });
+  },
+
+  /**
+   * Add sitemap map file | Log.
+   *
+   * @param {Object} data
+   * @param {String} map
+   * @param {Function} callback
+   */
+  addSitemapMap: (data, map, callback) => {
+    if (map && typeof map === 'string') {
+      map = path.resolve(map);
+      // Get map directory.
+      const dirname = path.dirname(map);
+      // Create map directory.
+      mkdirp(dirname, '0777', (error) => {
+        if (error) {
+          callback(error);
+        }
+
+        // Get map basename.
+        const basename = path.basename(map).split('.');
+        // Prepare [MAP].json file.
+        const mapJSON = path.join(dirname, `${basename[0]}.json`);
+        // Stream - create map file.
+        const stream = fs.createWriteStream(mapJSON);
+        // Stream - error event.
+        stream.on('error', (error) => {
+          callback(error);
+        });
+
+        // Stream - write in file.
+        stream.write(JSON.stringify(_map));
+
+        callack(null, mapJSON);
+      });
+    }
+    else {
+      // Log.
+      console.dir(data);
+    }
+  }
+};
