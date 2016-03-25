774f04a3adcabbf4e08966d609f92b9e989e79eb
lasha <lashacst@gmail.com>
2016-03-25 11:57:34 +0400

    a11y module update - make the code testable

--- modules/a11y.js
+++ modules/a11y.js
@@ -1,5 +1,5 @@
 /**
- * @file a11y-audit.js
+ * @file a11y.js
  * @author Lasha Badashvili (lashab@picktek.com)
  *
  * Accessibility testing - (using pa11y module).
@@ -18,14 +18,71 @@ const pa11y  = require('pa11y');
 const report = require('../helpers/report');
 const files  = require('../helpers/files');
 
-module.exports = (argv) => {
-  if (!process.env.PATH) {
-    throw new Error('Environment PATH not found.');
-  }
+module.exports = {
+  /**
+   * Execute fetch.
+   */
+  execute(argv) {
+    if (!process.env.PATH) {
+      throw new Error('Environment PATH not found.');
+    }
 
-  /*eslint-disable max-len*/
-  // Prepare help text.
-  const help = `html-audit a11y usage:
+    // Get arg - path to file.
+    const _path = argv.path;
+    // Get arg - report directory.
+    const _report = argv.report || '';
+    // Get arg - accessibility standard.
+    const standard = argv.standard || 'WCAG2AA';
+    // Get arg - ignore.
+    const ignore = argv.ignore || [];
+    // Get arg - JSON map file.
+    const map = argv.map;
+    // Get arg - modified boolean.
+    const modified = argv.lastmod || false;
+
+    if (argv.help || !_path || (modified && !map)) {
+      console.log(this.help());
+      process.exit(0);
+    }
+
+    // Prepare pa11y options.
+    const options = {
+      standard,
+      log:      {
+        debug: console.log.bind(console),
+        error: console.error.bind(console),
+        info:  console.info.bind(console)
+      },
+      phantom: {
+        path: phantomjs.path
+      }
+    };
+
+    if (ignore && typeof ignore === 'string') {
+      // Add ignore option.
+      options['ignore'] = ignore.split(',');
+    }
+
+    // Run pa11y.
+    this.pa11y(options, _path, argv._, map, modified, (error, data) => {
+      if (error) {
+        throw new Error();
+      }
+
+      // Create report.
+      report({
+        assessibility: data
+      }, _report, 'a11y-report.json');
+    });
+  },
+
+  /**
+   * Get help text.
+   */
+  help: () => {
+    /*eslint-disable max-len*/
+    // Prepare help text.
+    const help = `html-audit a11y usage:
         html-audit a11y [options]
 Options
         --help                                                           Display help text
@@ -34,73 +91,42 @@ Options
         --report    [path]                                               Path to output JSON audit report
         --ignore    [types]                                              Types to ignore separated by semi-colons (notice,warning)
         --map       [file]        (required when --lastmod is provided)  JSON map file which holds modified files data
-        --lastmod                                                        Scan last modified files
-`;
-  /*eslint-enable max-len*/
+        --lastmod                                                        Scan last modified files`;
+    /*eslint-enable max-len*/
 
-  if (argv.help) {
-    process.stdout.write(help.yellow);
-    process.exit(0);
+    return help.yellow;
+  },
+
+  /**
+   * Run pa11y.
+   *
+   * @param {Object} options
+   * @param {String} file
+   * @param {Object} _files
+   * @param {String} map
+   * @param {Boolean} modified
+   * @param {Function} callback
+   */
+  pa11y: (options, file, _files, map, modified, callback) => {
+    const _patty = pa11y(options);
+    const _data = {};
+    let i = 1;
+    // Get file(s).
+    files(file, _files, map, modified, (file, length) => {
+      // Prepare _data object.
+      _data[file] = _patty.run.bind(_patty, `file://${path.resolve(file)}`);
+      if (i === length) {
+        // Test file(s).
+        async.series(_data, (error, data) => {
+          if (error) {
+            callback(error);
+          }
+
+          callback(null, data);
+        });
+      }
+      i++;
+    });
   }
-
-  const _data = {};
-  // Get arg - path to file.
-  const _path = argv.path;
-  // Get arg - report directory.
-  const _report = argv.report || '';
-  // Get arg - accessibility standard.
-  const standard = argv.standard || 'WCAG2AA';
-  // Get arg - ignore.
-  let ignore = argv.ignore || [];
-  // Get arg - JSON map file.
-  const map = argv.map;
-  // Get arg - modified boolean.
-  const modified = argv.lastmod || false;
-
-  if (!_path || (modified && !map)) {
-    process.stdout.write(help.yellow);
-    process.exit(0);
-  }
-
-  // Prepare pa11y options.
-  const options = {
-    standard,
-    log:      {
-      debug: console.log.bind(console),
-      error: console.error.bind(console),
-      info:  console.info.bind(console)
-    },
-    phantom: {
-      path: phantomjs.path
-    }
-  };
-
-  if (ignore && typeof ignore === 'string') {
-    // Get ignore object.
-    ignore = ignore.split(';');
-    // Add ignore option.
-    options['ignore'] = ignore;
-  }
-
-  let i = 1;
-  const _patty = pa11y(options);
-  // Get file(s).
-  files(_path, argv._, map, modified, (file, length) => {
-    // Prepare _data object.
-    _data[file] = _patty.run.bind(_patty, `file://${path.resolve(file)}`);
-    if (i === length) {
-      // Test file(s).
-      async.series(_data, (error, data) => {
-        if (error) {
-          throw new Error(error);
-        }
-
-        // Create report.
-        report({
-          assessibility: data
-        }, _report, 'a11y-report.json');
-      });
-    }
-    i++;
-  });
 };
+
