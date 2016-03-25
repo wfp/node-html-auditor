8fd984958adde7ffb16602bb528a192af7a6f101
lasha <lashacst@gmail.com>
2016-03-25 13:28:16 +0400

    html5 module update - make the code testable

--- modules/html5.js
+++ modules/html5.js
@@ -17,79 +17,104 @@ const _ = require('underscore');
 const report = require('../helpers/report');
 const files = require('../helpers/files');
 
-module.exports = (argv) => {
-  /*eslint-disable max-len*/
-  // Prepare help text.
-  const help = `html-audit html5 usage:
-        html-audit html5 [options]
-Options
-        --help                                                                Display help text
-        --path           [path / file] (required)                             Path to HTML files or an HTML file to audit
-        --report         [path]                                               Path to output JSON audit report
-        --errors-only                                                         Only report errors (no notices or warnings)
-        --map            [file]        (required when --lastmod is provided)  JSON map file which holds modified files data
-        --lastmod                                                             Scan last modified files
-`;
-  /*eslint-enable max-len*/
+module.exports = {
+  /**
+   * Execute html5.
+   */
+  execute(argv) {
+    // Get arg - path to file.
+    const path = argv.path;
+    // Get arg - report directory.
+    const _report = argv.report || '';
+    // Get arg - errors only.
+    const errorsOnly = argv['errors-only'] || false;
+    // Get arg - JSON map file.
+    const map = argv.map || '';
+    // Get arg - modified boolean.
+    const modified = argv.lastmod || false;
 
-  if (argv.help) {
-    process.stdout.write(help.yellow);
-    process.exit(0);
-  }
+    if (argv.help || !path || (modified && !map)) {
+      console.log(this.help());
+      process.exit(0);
+    }
 
-  const _data = [];
-  // Get arg - path to file.
-  const path = argv.path;
-  // Get arg - report directory.
-  const _report = argv.report || '';
-  // Get arg - errors only.
-  const errorsOnly = argv['errors-only'] || false;
-  // Get arg -JSON map file.
-  const map = argv.map || '';
-  // Get arg - modified boolean.
-  const modified = argv.lastmod || false;
-
-  if (!path || (modified && !map)) {
-    process.stdout.write(help.yellow);
-    process.exit(0);
-  }
-
-  let i = 1;
-  // Get file(s).
-  files(path, argv._, map, modified, (file, length) => {
-    // Get file content.
-    fs.readFile(file, 'utf-8', (error, content) => {
+    this.html5Lint(path, argv._, map, modified, errorsOnly, (error, data) => {
       if (error) {
         throw new Error(error);
       }
 
-      // Test file.
-      html5Lint(content, { errorsOnly }, (error, data) => {
+      // Create report.
+      report({
+        html5: data
+      }, _report, 'html5-report.json');
+    });
+  },
+
+  /**
+   * Get help text.
+   */
+  help: () => {
+    /*eslint-disable max-len*/
+    const help = `html-audit html5 usage:
+        html-audit html5 [options]
+Options
+        --help                                                              Display help text
+        --path         [path / file] (required)                             Path to HTML files or an HTML file to audit
+        --report       [path]                                               Path to output JSON audit report
+        --errors-only                                                       Only report errors (no notices or warnings)
+        --map          [file]        (required when --lastmod is provided)  JSON map file which holds modified files data
+        --lastmod                                                           Scan last modified files`;
+    /*eslint-enable max-len*/
+
+    return help.yellow;
+  },
+
+  /**
+   * Run html5Lint.
+   *
+   * @param {String} file
+   * @param {Object} _files
+   * @param {String} map
+   * @param {Boolean} modified
+   * @param {Boolean} errorsOnly
+   * @param {Function} callback
+   */
+  html5Lint: (file, _files, map, modified, errorsOnly, callback) => {
+    const _data = [];
+    let i = 1;
+    // Get file(s).
+    files(file, _files, map, modified, (file, length) => {
+      // Get file content.
+      fs.readFile(file, 'utf-8', (error, content) => {
         if (error) {
-          throw new Error(error);
+          callback(error);
         }
 
-        if (data.messages.length) {
-          // Log file scanning.
-          console.log('Scanning %s'.green, file);
-          data.messages.forEach((object) => {
-            // Add filename.
-            object.filename = file;
-            // Store result in _data variable.
-            _data.push(object);
-          });
-
-          if (i === length) {
-            // Create report.
-            report({
-              html5: _.groupBy(_data, 'filename')
-            }, _report, 'html5-report.json');
+        // Test file.
+        html5Lint(content, { errorsOnly }, (error, data) => {
+          if (error) {
+            callback(error);
           }
 
-          i++;
-        }
+          if (data.messages.length) {
+            data.messages.forEach((object) => {
+              // Add filename.
+              object.filename = file;
+              // Store result in _data variable.
+              _data.push(object);
+            });
+
+            // Log file scanning.
+            console.log(`Test passed - ${file}`.green);
+
+            if (i === length) {
+              callback(null, _.groupBy(_data, 'filename'));
+            }
+
+            i++;
+          }
+        });
       });
     });
-  });
+  }
 };
-
