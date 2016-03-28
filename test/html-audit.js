6b654cd903f3ce079523e937610c0e4905b6aa78
lasha <lashacst@gmail.com>
2016-03-28 17:33:48 +0400

    Update tests #50

--- test/html-audit.js
+++ test/html-audit.js
@@ -17,38 +17,36 @@ describe('html-audit', function() {
     });
   });
 
-  context('scan', () => {
-    context('a11y', () => {
-      it(`should pass accessebility test`, (done) => {
-        require('../modules/a11y').execute({ 
-          _: [ 'a11y' ],
-          path: 'files/sitemaps/',
-          report: 'files/reports/',
-         }, done);
-      });
+  context('a11y', () => {
+    it(`should pass accessebility test`, (done) => {
+      require('../modules/a11y').execute({ 
+        _: [ 'a11y' ],
+        path: 'files/sitemaps/',
+        report: 'files/reports/',
+       }, done);
     });
+  });
 
-    context('html5', () => {
-      it(`should pass HTML5 validator test`, (done) => {
-        require('../modules/html5').execute({ 
-          _: [ 'html5' ],
-          path: 'files/sitemaps/',
-          report: 'files/reports/',
-          'errors-only': true
-         }, done);
-      });
+  context('html5', () => {
+    it(`should pass HTML5 validator test`, (done) => {
+      require('../modules/html5').execute({ 
+        _: [ 'html5' ],
+        path: 'files/sitemaps/',
+        report: 'files/reports/',
+        'errors-only': true
+       }, done);
     });
+  });
 
-    context('link', () => {
-      it(`should pass broken link checker test`, (done) => {
-        require('../modules/link').execute({ 
-          _: [ 'link' ],
-          path: 'files/sitemaps/',
-          report: 'files/reports/',
-          'base-uri': 'https://www.washingtonpost.com',
-          'report-verbose': true
-         }, done);
-      });
+  context('link', () => {
+    it(`should pass broken link checker test`, (done) => {
+      require('../modules/link').execute({ 
+        _: [ 'link' ],
+        path: 'files/sitemaps/',
+        report: 'files/reports/',
+        'base-uri': 'https://www.washingtonpost.com',
+        'report-verbose': true
+       }, done);
     });
   });
 });
